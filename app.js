fff9444b2e42653c38907dd53ebeeb91c2d41b02
Dmitriy Gorbatenko <gorbatenko-2000@bk.ru>
2022-02-17 17:17:52 +0300

    feat: Добавить поддержку cors, логирование

--- app.js
+++ app.js
@@ -3,18 +3,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const authMiddleware = require('./middlewares/auth.middleware');
+const corsMiddleware = require('./middlewares/cors.middleware');
 const userRouter = require('./routes/user.router');
 const cardRouter = require('./routes/card.router');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
 app.use(express.json());
+app.use(requestLogger);
+app.use(corsMiddleware);
 
 app.use('/users', userRouter);
 app.use('/cards', authMiddleware, cardRouter);
 app.use((req, res) => {
   res.status(404).json({ message: 'Запрашиваемый ресурс не найден' });
 });
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use((error, req, res, next) => { // eslint-disable-line no-unused-vars
@@ -36,7 +42,7 @@ app.use((error, req, res, next) => { // eslint-disable-line no-unused-vars
 const start = async () => {
   try {
     await mongoose.connect('mongodb://localhost:27017/mestodb');
-    app.listen(3000, () => console.log('Server start'));
+    app.listen(5000, () => console.log('Server start'));
   } catch (error) {
     console.log(error);
   }
